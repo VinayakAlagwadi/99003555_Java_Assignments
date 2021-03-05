985947ce5b7f4d95997fde4d18811f174362d422
VinayakAlagwadi <78849168+VinayakAlagwadi@users.noreply.github.com>
2021-03-05 08:50:40 +0530

    Add files via upload

---
+++ Activity 8/set/HashSet1.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSet1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		HashSet hs=new HashSet();
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			String name=sc.next();
+			hs.add(name);
+		}
+		System.out.println(hs.size());
+		
+	}
+
+	private char[] size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void add(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
